03b6908dce5a5261fcf906b45b6ca44742f03f43
Divyanshu-Modi <divyan.m05@gmail.com>
2025-01-06 02:25:59 +0530

    ESP_ROBOTICS: Add initial infra for builds
    Change-Id: I14f7bdaba5157689a25a1573693a92b77d362b15
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

---
+++ main/main.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// Copyright(c) Divyanshu-Modi <divyan.m05@gmail.com>
+
+#include "esp_err.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+	return;
+}
